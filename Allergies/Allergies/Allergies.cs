661c63f2d66d4518b1035a7243e7903080fc6ccc
ginaisvor <ginaisvor@yahoo.com>
2018-12-17 19:08:46 +0200

    Tema 4 OOP

---
+++ Allergies/Allergies/Allergies.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Allergies
+{
+    public enum Allergens { Eggs = 1, Peanuts = 2, Shelfish = 4, Strawberries = 8, Tomatoes = 16, Chocolate = 32, Pollen = 64, Cats = 128 };
+
+    public class Allergies
+    {
+        private string name;
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+
+
+        public void GetAllergens(int score)
+        {
+            int indice = score;
+            if (score >= 256)
+            {
+                Console.WriteLine(" {0} is alergic to: {1}", this.name, Enum.GetName(typeof(Allergens), 1));
+            }
+            else
+            {
+                Console.Write(this.name + " is alergic to: ");
+                while (indice >= 1)
+                {
+                    int min = 0;
+                    int wanted = 0;
+                    foreach (int num in Enum.GetValues(typeof(Allergens)))
+                    {
+                        if (num > min && num <= indice)
+                        {
+                            wanted = num;
+                            min = num;
+                        }
+                    }
+                    Console.Write(Enum.GetName(typeof(Allergens), wanted) + " ");
+                    indice -= wanted;
+
+                }
+                Console.WriteLine();
+
+            }
+        }
+    }
+}
---
+++ Allergies/Allergies/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Allergies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Allergies obj = new Allergies();
+            obj.Name = "Gina";
+            obj.GetAllergens(9);
+        }
+    }
+}
